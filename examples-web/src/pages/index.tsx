55345eca500747a5195dfdd29500ec6743803471
zhuharvey <zhuharvey@gmail.com>
2023-05-20 16:41:44 -0400

    added assets

--- examples-web/src/pages/index.tsx
+++ examples-web/src/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <div>
       <h1 className="text-4xl font-medium text-center">Axelar Web Examples</h1>
         <div className="flex flex-wrap justify-center mt-8">
-          <Card
+          {/* <Card
             classname="mx-2 my-2"
             title="Send message to another chain"
             description="Transfer messages from the source chain to the destination chain using the Axelar Gateway's callContract function."
@@ -18,13 +18,7 @@ const Home: NextPage = () => {
             title="Send token to another chain"
             description="Simply sending a token from source chain to the destination chain"
             url="/examples/send-tokens"
-          />
-          <Card
-            classname="mx-2 my-2"
-            title="Send token with custom logic to another chain"
-            description="Transfer tokens and execute custom logic to the destination chain using the callContractWithToken function of the Axelar Gateway contract."
-            url="/examples/call-contract-with-token"
-          />
+          /> */}
           <Card
             classname="mx-2 my-2"
             title="Send NFTs Between Chains"
