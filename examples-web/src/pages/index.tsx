00152340f2ed441cbcc39a48def825a392bc772a
zhuharvey <zhuharvey@gmail.com>
2023-05-20 19:20:51 -0400

    Update index.tsx

--- examples-web/src/pages/index.tsx
+++ examples-web/src/pages/index.tsx
@@ -3,28 +3,68 @@ import type { NextPage } from "next";
 import React from "react";
 
 const Home: NextPage = () => {
+  const handleBuyTickets = () => {
+    const element = document.getElementById('upcomingEvents');
+    element?.scrollIntoView({behavior: "smooth"});
+  };
+
   return (
-    <div>
-      <h1 className="text-4xl font-medium text-center">Axelar Web Examples</h1>
+    <div className="flex flex-col min-h-screen justify-between">
+      <div className="flex flex-col items-center justify-center pt-20 pb-10 px-4">
+        <h1 className="text-4xl font-medium text-center mb-5">Welcome to TickeX</h1>
+        <p className="text-2xl font-medium text-center mb-5">Your one-stop platform for secure and easy ticket transactions.</p>
+        <p className="text-2xl font-medium text-center mb-5">Trade tickets freely on our NFT marketplace. No worries, no hassles.</p>
+        <div className="mb-10 flex">
+          <button className="mr-4 px-4 py-2 border-2 border-black font-bold">CREATE EVENT</button>
+          <button onClick={handleBuyTickets} className="px-4 py-2 border-2 border-black font-bold">BUY TICKETS</button>
+        </div>
+      </div>
+      <div id="upcomingEvents" className="w-full py-10 px-4 bg-gray-200">
+        <h1 className="text-xl font-medium text-left mb-5">Upcoming Events</h1>
         <div className="flex flex-wrap justify-center mt-8">
-          {/* <Card
-            classname="mx-2 my-2"
-            title="Send message to another chain"
-            description="Transfer messages from the source chain to the destination chain using the Axelar Gateway's callContract function."
-            url="/examples/call-contract"
-          />
-          <Card
-            classname="mx-2 my-2"
-            title="Send token to another chain"
-            description="Simply sending a token from source chain to the destination chain"
-            url="/examples/send-tokens"
-          /> */}
-          <Card
-            classname="mx-2 my-2"
-            title="Send NFTs Between Chains"
-            description="Send a NFT token from source chain to the destination chain"
-            url="/examples/nft-linker"
-          />
+            <Card
+              classname="mx-2 my-2"
+              title="Send NFTs Between Chains"
+              description="Send a NFT token from source chain to the destination chain"
+              url="/examples/nft-linker"
+            />
+            <Card
+              classname="mx-2 my-2"
+              title="Send NFTs Between Chains"
+              description="Send a NFT token from source chain to the destination chain"
+              url="/examples/nft-linker"
+            />
+            <Card
+              classname="mx-2 my-2"
+              title="Send NFTs Between Chains"
+              description="Send a NFT token from source chain to the destination chain"
+              url="/examples/nft-linker"
+            />
+            <Card
+              classname="mx-2 my-2"
+              title="Send NFTs Between Chains"
+              description="Send a NFT token from source chain to the destination chain"
+              url="/examples/nft-linker"
+            />
+            <Card
+              classname="mx-2 my-2"
+              title="Send NFTs Between Chains"
+              description="Send a NFT token from source chain to the destination chain"
+              url="/examples/nft-linker"
+            />
+            <Card
+              classname="mx-2 my-2"
+              title="Send NFTs Between Chains"
+              description="Send a NFT token from source chain to the destination chain"
+              url="/examples/nft-linker"
+            />
+            <Card
+              classname="mx-2 my-2"
+              title="Send NFTs Between Chains"
+              description="Send a NFT token from source chain to the destination chain"
+              url="/examples/nft-linker"
+            />
+        </div>
       </div>
     </div>
   );
